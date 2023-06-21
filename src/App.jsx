857aa2d14efbb888ce30d03aab0475d8eb40f41f
Lucas Arteaga <lucas.arteaga16@gmail.com>
2023-06-21 14:50:57 -0300

    Se Agregó la card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
 import { Input } from "./Components/Input/Input"
+import { DataCard } from './Components/Card/DataCard';
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [weatherData,setWeatherData] = useState({});
+  const [error, setError] = useState(false);
 
   return (
     <>
       <h1>Clima App ☁</h1>
-      <Input></Input>
+      <main>
+      <Input setWeatherData={setWeatherData} setError={setError}></Input>
+      {
+        !error ? (
+          <DataCard weatherData={weatherData}></DataCard>
+        ) : (
+          <Error></Error>
+        )
+      }
+      
+      {/* 
+        ESTO SERIA LO MISMO QUE LO DE ARRIBA, ES UN TERNARIO
+        {
+        if (!error) {
+          <Card weatherData={weatherData}></Card>
+        }else{
+          <Error></Error>
+        }
+      } */}
+      </main>
+      <footer>
+        <p>&copy : Todos los derechos reservados</p>
+      </footer>
     </>
   )
 }
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 
-export const Input = () => {
+export const Input = ({setWeatherData,setError}) => {
+    const [city , setCity] = useState("San Miguel de Tucumán")
+    const [country , setCountry] = useState("AR");
+    const API = process.env.REACT_APP_APIKEY;
 
+
+    useEffect(() => {
+        queryAPI();
+     }, []);
+
+    const queryAPI = async() =>{
+        try{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&lang=es&appid=${API}&units=metric`)
+            const data = await response.json();
+            if(response.status === 200){
+              setWeatherData(data);
+              setError(false)
+            }else{
+              setError(true)
+            }
+        }catch(error)
+        {
+            console.log('error al consultar la api')
+        }
+      }
     const handleSubmit = (e) => {
         e.preventDeFault()
         queryAPI();
@@ -16,22 +40,26 @@ export const Input = () => {
         <Form onSubmit={handleSubmit}>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+          <Form.Label>Ciudad</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="First name"
             defaultValue="Mark"
+            onChange={(e) => setCity(e.target.value)}
+            value = {city}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+          <Form.Label>Código de país</Form.Label>
           <Form.Control
             required
             type="text"
             placeholder="Last name"
             defaultValue="Otto"
+            onChange={(e) => setCountry(e.target.value)}
+            value = {country}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
